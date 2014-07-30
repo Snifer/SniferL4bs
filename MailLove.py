ac94a81b778e3e056d73a47238f455ab81f8250f
w. chorolque <wchorolque@gmail.com>
2014-07-30 04:27:35 -0400

    Agregado lectura de datos desde variables de entorno

--- MailLove.py
+++ MailLove.py
@@ -1,49 +1,50 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
-#maillove.py
+# maillove.py
 #Realizado por Jose Moruno Cadima a.k.a Snifer
 # URL: http://www.sniferl4bs.com/2014/07/automatizacion-reconquistando-la-novia.html
 # www.sniferl4bs.com
- 
+
 import smtplib
 import mimetypes
 import os
 import sys
- 
+
 from email.MIMEMultipart import MIMEMultipart
 from email.Encoders import encode_base64
 from email.mime.text import MIMEText
 from email.MIMEBase import MIMEBase
 from email import encoders
- 
-gmail_to = 'xxxx@gmail.com' #Correo a enviar
-gmail_from = 'xxxxxx@gmail.com' #Correo de donde se envia
-password='*******' #Contraseña
- 
-os.system("rm /tmp/log")
+
+# export MAIL_FROM='mail_from@mail.com'
+# export MAIL_PASS='password mail'
+# export MAIL_TO='mail_to@mail.com'
+mail_from = os.environ.get('MAIL_FROM')
+mail_pass = os.environ.get('MAIL_PASS')
+mail_to = os.environ.get('MAIL_TO')
+fortune_file = 'computers'
+cowsay_img = 'kiss'
+
+if True == os.path.exists('/tmp/mail_log.txt'):
+    os.remove('/tmp/mail_log.txt')
 #os.system("acpi -i | tee /tmp/log.txt")<
-os.system("fortune rizel | cowsay -f kiss | tee /tmp/log")
- 
- 
-msg = MIMEMultipart()
-msg['From']= (gmail_from)
-msg['To']= (gmail_to)
-msg['Subject']="Subject"
- 
- 
-file = open("/tmp/log.txt", "rb") #Direccion del Archivo
+os.system("fortune %s | cowsay -f %s | tee /tmp/mail_log.txt" % (fortune_file, cowsay_img))
+
 attach_file = MIMEBase('multipart', 'encrypted')
-attach_file.set_payload(file.read())
-file.close()
- 
-attach_file.add_header('Content-Disposition', 'attachment', filename='Una Frase al Día.txt')#NOMBRE DEL ARCHIVO A ENVIAR QUE SE VERA
+with open('/tmp/mail_log.txt', 'rb') as file:  #Direccion del Archivo
+    attach_file.set_payload(file.read())
+    attach_file.add_header('Content-Disposition', 'attachment', filename='Una Frase al Día.txt')
+
+msg = MIMEMultipart()
+msg['From'] = (mail_from)
+msg['To'] = (mail_to)
+msg['Subject'] = 'Una frase al dia'
 msg.attach(attach_file)
- 
- 
-mailServer = smtplib.SMTP('smtp.gmail.com',587)
+
+mailServer = smtplib.SMTP('smtp.gmail.com', 587)
 mailServer.ehlo()
 mailServer.starttls()
 mailServer.ehlo()
-mailServer.login(gmail_from, password)
-mailServer.sendmail(gmail_from, gmail_to, msg.as_string())
+mailServer.login(mail_from, mail_pass)
+mailServer.sendmail(mail_from, mail_to, msg.as_string())
 mailServer.close()
