0bb922f79d3c987ab0b2e2523a760e375945773e
w. chorolque <wchorolque@gmail.com>
2014-08-01 00:57:01 -0400

    Agregado parametros al script

--- MailLove.py
+++ MailLove.py
@@ -5,22 +5,47 @@
 # URL: http://www.sniferl4bs.com/2014/07/automatizacion-reconquistando-la-novia.html
 # www.sniferl4bs.com
 
-import mimetypes
+import argparse
 import os
 import smtplib
-import sys
 
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 
-# export MAIL_FROM='mail_from@mail.com'
-# export MAIL_PASS='password mail'
-# export MAIL_TO='mail_to@mail.com'
-mail_from = os.environ.get('MAIL_FROM')
-mail_pass = os.environ.get('MAIL_PASS')
-mail_to = os.environ.get('MAIL_TO')
-fortune_file = 'computers'
-cowsay_img = 'kiss'
+parser = argparse.ArgumentParser(description='Send a mail generated with cowsay and fortune')
+parser.add_argument('-f', '--fortune', default='computers', help='Name of the fortune file (default: %(default)s)')
+parser.add_argument('-c', '--cowfile', default='koala', help='Name of the cowsay file (default: %(default)s)')
+parser.add_argument('-s', '--sender', help='The value of the From: header')
+parser.add_argument('-r', '--recipient', help='The value of the To: header')
+parser.add_argument('-p', '--password', help='Email password')
+
+args = parser.parse_args()
+
+print args
+
+mail_from = args.sender
+mail_pass = args.password
+mail_to = args.recipient
+fortune_file = args.fortune
+cowsay_img = args.cowfile
+
+if None == mail_from:
+    # export MAIL_FROM='mail_from@mail.com'
+    mail_from = os.environ.get('MAIL_FROM')
+    if None == mail_from:
+        raise Exception('Error: mail From')
+
+if None == mail_pass:
+    # export MAIL_PASS='password mail'
+    mail_pass = os.environ.get('MAIL_PASS')
+    if None == mail_pass:
+        raise Exception('Error: mail Password')
+
+if None == mail_to:
+    # export MAIL_TO='mail_to@mail.com'
+    mail_to = os.environ.get('MAIL_TO')
+    if None == mail_to:
+        raise Exception('Error: mail To')
 
 if True == os.path.exists('/tmp/mail_log.txt'):
     os.remove('/tmp/mail_log.txt')
@@ -30,7 +55,7 @@ os.system("fortune %s | cowsay -f %s | tee /tmp/mail_log.txt" % (fortune_file, c
 attach_file = MIMEBase('multipart', 'encrypted')
 with open('/tmp/mail_log.txt', 'rb') as file:  #Direccion del Archivo
     attach_file.set_payload(file.read())
-    attach_file.add_header('Content-Disposition', 'attachment', filename='Una Frase al Día.txt')
+    attach_file.add_header('Content-Disposition', 'attachment', filename='Una Frase al Dia.txt')
 
 msg = MIMEMultipart()
 msg['From'] = (mail_from)
