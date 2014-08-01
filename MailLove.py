cc4fb989621111616a2e03566ae45a9835bf996e
w. chorolque <wchorolque@gmail.com>
2014-08-01 02:05:22 -0400

    Agregado Try-Except

--- MailLove.py
+++ MailLove.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 # maillove.py
-#Realizado por Jose Moruno Cadima a.k.a Snifer
+# Realizado por Jose Moruno Cadima a.k.a Snifer
 # URL: http://www.sniferl4bs.com/2014/07/automatizacion-reconquistando-la-novia.html
 # www.sniferl4bs.com
 
@@ -14,33 +14,41 @@ from email.mime.multipart import MIMEMultipart
 
 
 def generate_attach(fortune_file, cowsay_img):
-    if True == os.path.exists('/tmp/mail_log.txt'):
-        os.remove('/tmp/mail_log.txt')
+    try:
+        if True == os.path.exists('/tmp/mail_log.txt'):
+            os.remove('/tmp/mail_log.txt')
 
-    os.system("fortune %s | cowsay -f %s | tee /tmp/mail_log.txt" % (fortune_file, cowsay_img))
+        os.system("fortune %s | cowsay -f %s | tee /tmp/mail_log.txt" % (fortune_file, cowsay_img))
 
-    attach_file = MIMEBase('multipart', 'encrypted')
-    with open('/tmp/mail_log.txt', 'rb') as file:  #Direccion del Archivo
-        attach_file.set_payload(file.read())
-        attach_file.add_header('Content-Disposition', 'attachment', filename='Una Frase al Dia.txt')
+        attach_file = MIMEBase('multipart', 'encrypted')
+        with open('/tmp/mail_log.txt', 'rb') as file:  # Direccion del Archivo
+            attach_file.set_payload(file.read())
+            attach_file.add_header('Content-Disposition', 'attachment', filename='Una Frase al Dia.txt')
 
-    return attach_file
+        return attach_file
+    except Exception as e:
+        raise Exception(e)
+
 
 def send_mail(mail_from, mail_pass, mail_to, fortune_file, cowsay_img):
-    attach_file = generate_attach(fortune_file, cowsay_img)
-    msg = MIMEMultipart()
-    msg['From'] = (mail_from)
-    msg['To'] = (mail_to)
-    msg['Subject'] = 'Una frase al dia'
-    msg.attach(attach_file)
+    try:
+        attach_file = generate_attach(fortune_file, cowsay_img)
+        msg = MIMEMultipart()
+        msg['From'] = (mail_from)
+        msg['To'] = (mail_to)
+        msg['Subject'] = 'Una frase al dia'
+        msg.attach(attach_file)
 
-    mailServer = smtplib.SMTP('smtp.gmail.com', 587)
-    mailServer.ehlo()
-    mailServer.starttls()
-    mailServer.ehlo()
-    mailServer.login(mail_from, mail_pass)
-    mailServer.sendmail(mail_from, mail_to, msg.as_string())
-    mailServer.close()
+        mailServer = smtplib.SMTP('smtp.gmail.com', 587)
+        mailServer.ehlo()
+        mailServer.starttls()
+        mailServer.ehlo()
+        mailServer.login(mail_from, mail_pass)
+        mailServer.sendmail(mail_from, mail_to, msg.as_string())
+        mailServer.close()
+    except Exception as e:
+        raise Exception(e)
+
 
 def main():
     parser = argparse.ArgumentParser(description='Send a mail generated with cowsay and fortune')
@@ -59,24 +67,24 @@ def main():
     cowsay_img = args.cowfile
 
     if None == mail_from:
-        # export MAIL_FROM='mail_from@mail.com'
-        mail_from = os.environ.get('MAIL_FROM')
+        mail_from = os.environ.get('MAIL_FROM')  # export MAIL_FROM='mail_from@mail.com'
         if None == mail_from:
             raise Exception('Error: mail From')
 
     if None == mail_pass:
-        # export MAIL_PASS='password mail'
-        mail_pass = os.environ.get('MAIL_PASS')
+        mail_pass = os.environ.get('MAIL_PASS')  # export MAIL_PASS='password mail'
         if None == mail_pass:
             raise Exception('Error: mail Password')
 
     if None == mail_to:
-        # export MAIL_TO='mail_to@mail.com'
-        mail_to = os.environ.get('MAIL_TO')
+        mail_to = os.environ.get('MAIL_TO')  # export MAIL_TO='mail_to@mail.com'
         if None == mail_to:
             raise Exception('Error: mail To')
 
-    send_mail(mail_from, mail_pass, mail_to, fortune_file, cowsay_img)
+    try:
+        send_mail(mail_from, mail_pass, mail_to, fortune_file, cowsay_img)
+    except Exception as e:
+        print 'Error: %s ' % e.message
 
 
 if '__main__' == __name__:
